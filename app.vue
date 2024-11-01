1863bbed688bf1215ef98f785b704e0beab806cd
Marumasa <77374813+malken21@users.noreply.github.com>
2024-11-01 17:06:25 +0900

    init

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="black-background">
+    <p>TypeScriptつかえ</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+.black-background {
+  background-color: black;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+p {
+  color: white;
+  font-size: 2em;
+}
+</style>
